addc736bf9f1fe6e3e6755e8dd54dbe3f07b4786
tarunpatil01 <tarunpatil09876@gmail.com>
2024-12-20 09:22:52 +0530

    uploaded all new files

--- app/page.js
+++ app/page.js
@@ -1,101 +1,133 @@
+"use client";
+import React, { useEffect } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  useEffect(() => {
+    const video = document.getElementById("youtube-video");
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            video.src =
+              "https://www.youtube.com/embed/Uzy2yD3kIXc?si=rF54ELbusRDyrj8r&autoplay=1&vq=hd1080";
+            observer.unobserve(video);
+          }
+        });
+      },
+      { threshold: 0.5 }
+    );
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    observer.observe(video);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
+  return (
+    <>
+      <div className="flex justify-center flex-col gap-4 items-center text-white h-[44vh] px-5 md:px-0 text-xs md:text-base  ">
+        <div className="font-bold flex gap-6 md:gap-20 md:text-5xl justify-center items-center text-3xl">
+          Get Me a Chai{" "}
+          <span>
+            <img className="invertImg" src="/tea.gif" width={88} alt="" />
+          </span>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <p className="text-center md:text-left">
+          A crowdfunding platform for creators to fund their projects.
+        </p>
+        <p className="text-center md:text-left">
+          A place where your fans can buy you a chai. Unleash the power of your
+          fans and get your projects funded.
+        </p>
+        <div>
+          <Link href={"/login"}>
+            <button
+              type="button"
+              className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+            >
+              Start Here
+            </button>
+          </Link>
+
+          <Link href="/about">
+            <button
+              type="button"
+              className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+            >
+              Read More
+            </button>
+          </Link>
+        </div>
+      </div>
+      <div className="bg-white h-1 opacity-10"></div>
+
+      <div className="text-white container mx-auto pb-32 pt-14 px-10">
+        <h2 className="text-3xl font-bold text-center mb-14">
+          Your Fans can buy you a Chai
+        </h2>
+        <div className="flex gap-5 justify-around">
+          <div className="item space-y-3 flex flex-col items-center justify-center">
+            <img
+              className="bg-slate-400 rounded-full p-2 text-black"
+              width={88}
+              src="/man.gif"
+              alt=""
+            />
+            <p className="font-bold text-center">Fans want to help</p>
+            <p className="text-center">
+              Your fans are available to support you
+            </p>
+          </div>
+          <div className="item space-y-3 flex flex-col items-center justify-center">
+            <img
+              className="bg-slate-400 rounded-full p-2 text-black"
+              width={88}
+              src="/coin.gif"
+              alt=""
+            />
+            <p className="font-bold text-center">Fans want to contribute</p>
+            <p className="text-center">
+              Your fans are willing to contribute financially
+            </p>
+          </div>
+          <div className="item space-y-3 flex flex-col items-center justify-center">
+            <img
+              className="bg-slate-400 rounded-full p-2 text-black"
+              width={88}
+              src="/group.gif"
+              alt=""
+            />
+            <p className="font-bold text-center">Fans want to collaborate</p>
+            <p className="text-center">
+              Your fans are ready to collaborate with you
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="bg-white h-1 opacity-10"></div>
+
+      <div className="text-white container mx-auto pb-32 pt-14 flex flex-col items-center justify-center">
+        <h2 className="text-3xl font-bold text-center mb-14">
+          Learn more about us
+        </h2>
+        {/* Responsive youtube embed  */}
+        <div className="w-[90%] h-[40vh] md:w-[50%] md:h-[40vh] lg:w-[50%] lg:h-[40vh] xl:w-[50%] xl:h-[40vh]">
+          <iframe
+            id="youtube-video"
+            width="660"
+            height="405"
+            src="https://www.youtube.com/embed/Uzy2yD3kIXc?si=rF54ELbusRDyrj8r"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          ></iframe>
+        </div>
+      </div>
+    </>
   );
 }
