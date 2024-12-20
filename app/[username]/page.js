f902b800e823d4293c8be4096c19ba4f8bbb6f52
tarunpatil01 <tarunpatil09876@gmail.com>
2024-12-20 16:32:43 +0530

    fixed @

--- app/[username]/page.js
+++ app/[username]/page.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PaymentPage from '@/components/PaymentPage'
 import { notFound } from "next/navigation"
-import connectDb from '@/db/connectDb'
+import connectDb from '/db/connectDb'
 import User from '@/models/User'
 const Username = async ({ params }) => {
 
